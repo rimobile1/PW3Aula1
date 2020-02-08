dc8af23f02b7f6fd12a4f1445e8638b4d6a76828
rimobile1 <icaro.mo1234@gmail.com>
2020-02-07 22:31:05 -0300

    AulaDeJava1

---
+++ src/Nadador.java
@@ -0,0 +1,30 @@
+
+public class Nadador {
+	public static void main(String [] args){
+
+		int  idade = 16;
+		
+		if(idade >5 && idade <=7){ 
+			System.out.println("INFATIL");
+			}
+		else if(idade >= 8 && idade <=10){
+			System.out.println("JUVENIL");
+		}
+		else if	(idade >= 11 && idade <=15) {
+			System.out.println("ADOLECENTE");
+		}
+		else if	(idade >= 16 && idade <=30) {
+			System.out.println("ADULTO");
+		}
+		else if	(idade >30) {
+			System.out.println("SÊNIOR");
+		}
+		 
+	
+			
+		
+		
+		
+}
+	
+}
---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		String nome = "Icaro ";
+		String sobrenome = "Maia";
+		
+		System.out.println("Olá," + " " + nome + " " + sobrenome);
+		
+		System.out.println(String.format("Olá, %s %s ", nome, sobrenome ));
+
+	}
+	
+	
+
+	}
+
